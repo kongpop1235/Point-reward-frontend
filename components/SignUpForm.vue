a50a9717d8734c77596864fa6dd3d1621092e1af
Kongpop(Kai) <ru.kongpop@hotmail.com>
2024-12-16 17:09:42 +0700

    [EDIT] Set input username, password and name
    -Set character limit
    -Set the character limit that can be entered.

--- components/SignUpForm.vue
+++ components/SignUpForm.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
     <form @submit.prevent="handleSignUp">
-      <div class="mb-4">
+      <!-- Username Input -->
+      <div class="mb-4 relative">
         <label class="block mb-2" for="signup-username">{{ $t('forms.username') }}</label>
-        <input
-          v-model="signupUsername"
-          type="text"
-          id="signup-username"
-          class="w-full border rounded-md p-2 bg-darkGray text-white"
-          required
-        />
+        <div class="relative">
+          <input
+            v-model="signupUsername"
+            type="text"
+            id="signup-username"
+            class="w-full border rounded-md p-2 bg-darkGray text-white"
+            maxlength="20"
+            required
+            @input="validateUsername"
+          />
+          <div class="absolute right-2 top-1/2 transform -translate-y-1/2 text-white text-sm">
+            {{ signupUsername.length }}/20
+          </div>
+        </div>
       </div>
+
+      <!-- Password Input -->
       <div class="mb-4 relative">
         <label class="block mb-2" for="signup-password">{{ $t('forms.password') }}</label>
         <div class="relative">
@@ -19,34 +29,43 @@
             :type="isPasswordVisible ? 'text' : 'password'"
             id="signup-password"
             class="w-full border rounded-md p-2 bg-darkGray text-white"
+            maxlength="20"
             required
+            @input="validatePassword"
           />
-          <!-- Icon Show/hide password -->
           <div 
             class="absolute top-2 right-3 w-6 h-6 cursor-pointer"
             @click="togglePasswordVisibility"
           >
-            <eyeIcon 
-              v-if="isPasswordVisible" 
-              class="w-6 h-6 transition-all duration-300 text-white group-hover:translate-y-[-10px]"
-            />
-            <eyeOffIcon 
-              v-else 
-              class="w-6 h-6 transition-all duration-300 text-white group-hover:translate-y-[-10px]"
-            />
+            <eyeIcon v-if="isPasswordVisible" class="w-6 h-6 text-white" />
+            <eyeOffIcon v-else class="w-6 h-6 text-white" />
+          </div>
+          <div class="absolute right-11 top-1/2 transform -translate-y-1/2 text-white text-sm">
+            {{ signupPassword.length }}/20
           </div>
         </div>
       </div>
-      <div class="mb-4">
+
+      <!-- Name Input -->
+      <div class="mb-4 relative">
         <label class="block mb-2" for="signup-name">{{ $t('forms.name') }}</label>
-        <input
-          v-model="signupName"
-          type="text"
-          id="signup-name"
-          class="w-full border rounded-md p-2 bg-darkGray text-white"
-          required
-        />
+        <div class="relative">
+          <input
+            v-model="signupName"
+            type="text"
+            id="signup-name"
+            class="w-full border rounded-md p-2 bg-darkGray text-white"
+            maxlength="50"
+            required
+            @input="validateName"
+          />
+          <div class="absolute right-2 top-1/2 transform -translate-y-1/2 text-white text-sm">
+            {{ signupName.length }}/50
+          </div>
+        </div>
       </div>
+
+      <!-- Submit Button -->
       <div class="w-full text-center">
         <button
           type="submit"
@@ -70,14 +89,33 @@ const signupPassword = ref('')
 const signupName = ref('')
 const router = useRouter()
 
-// State For showing/hiding password
 const isPasswordVisible = ref(false)
 import eyeIcon from '@/assets/icon/eye.svg'
 import eyeOffIcon from '@/assets/icon/eye-off.svg'
 
-// Toggle function for displaying passwords
+// Toggle password visibility
 const togglePasswordVisibility = () => {
   isPasswordVisible.value = !isPasswordVisible.value
+}
+
+const validateUsername = () => {
+  signupUsername.value = signupUsername.value.replace(/[^a-zA-Z0-9_.]/g, '')
+  if (signupUsername.value.length > 20) {
+    signupUsername.value = signupUsername.value.slice(0, 20)
+  }
+}
+
+const validatePassword = () => {
+  if (signupPassword.value.length > 20) {
+    signupPassword.value = signupPassword.value.slice(0, 20)
+  }
+}
+
+const validateName = () => {
+  signupName.value = signupName.value.replace(/[^a-zA-Zก-๙\s]/g, '')
+  if (signupName.value.length > 50) {
+    signupName.value = signupName.value.slice(0, 50)
+  }
 }
 
 const handleSignUp = async () => {
